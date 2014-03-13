ab7559c2b919bf8de7ea74c9fcdec23b46cc979e
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-03-12 22:29:19 -0400

    Remove dbus-launch from gconf commands.

--- providers/default.rb
+++ providers/default.rb
@@ -1,12 +1,12 @@
 
 # Get the current value for the specified setting
 def get_current_gconf_value(user, confkey)
-  output = `sudo -u #{user} dbus-launch gconftool-2 --get #{confkey}`
+  output = `sudo -u #{user} gconftool-2 --get #{confkey}`
   return output.strip()
 end
 
 action :set do
-  cmd = "dbus-launch gconftool-2 --set #{new_resource.key} --type #{new_resource.attr_type} '#{new_resource.attr_val}'"
+  cmd = "gconftool-2 --set #{new_resource.key} --type #{new_resource.attr_type} '#{new_resource.attr_val}'"
   if 'list' == new_resource.attr_type then
     cmd += " --list-type #{new_resource.list_type}"
   end
@@ -24,10 +24,10 @@ end
 
 action :delete do
   if new_resource.recursive then
-    cmd = "dbus-launch gconftool-2 --recursive-unset #{new_resource.key}"
+    cmd = "gconftool-2 --recursive-unset #{new_resource.key}"
     cmd_name = "Recursively unsetting gconf key '#{new_resource.key}'"
   else
-    cmd = "dbus-launch gconftool-2 --unset #{new_resource.key}"
+    cmd = "gconftool-2 --unset #{new_resource.key}"
     cmd_name = "Unsetting gconf key '#{new_resource.key}'"
   end
   execute cmd_name do
